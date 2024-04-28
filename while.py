385f53961ba9f51f29655b418e2dc2140c7953af
joweeell <oswelandrade@outlook.es>
2024-04-27 23:15:40 -0400

    Repositorio del profe

--- while.py
+++ while.py
@@ -40,7 +40,7 @@ while mantener:
   print("2.- Programa de saludo")
   print("3.- Programa dormir")
   print("4.- Salir")
-  op=int(input("Seleccione una opcion"))
+  op=int(input("Seleccione un a opcion"))
   match op:
     case 1:
       verifpassw()
